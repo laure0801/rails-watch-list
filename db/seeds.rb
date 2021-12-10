25efd5ea247731f7874a4b6345cd025239b4a517
laure reina <laure.reina@gmail.com>
2021-12-10 14:46:19 +0100

    essai

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Movie.destroy_all
+
+wonder = {title: "Wonder Woman 1984", overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6.9}
+shank = {title: "The Shawshank Redemption", overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison", poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", rating: 8.7}
+titanic = {title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9}
+ocean = {title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0}
+
+
+[wonder, shank, titanic, ocean].each do |attributes|
+  movie = Movie.create!(attributes)
+  puts "Created #{movie.title}"
+end
+
+List.create(name: 'Drama')
+  List.create(name: 'Comedy')
+  List.create(name: 'Sci-fi')
+  List.create(name: 'Superhero')
+
+  100.times do
+    b = Bookmark.new(comment: Time.now, movie: Movie.all.sample, list: List.all.sample)
+    b.save
+    puts 'Creating 1 bookmark...'
+  end
